38086f558ecf9652ec0d58d1a1590f72bdb4a50b
Michal Madejski <michalmadejski00@gmail.com>
2018-09-07 19:39:43 +0200

    Dodano drugi commit HelloWorld gitignore

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Pierwszy commit");
+        System.out.println("Drugi commit");
     }
 }
