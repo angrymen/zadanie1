3e18aec23e13602c5050171c60c76474c75a6c01
Michal Madejski <michalmadejski00@gmail.com>
2018-09-07 19:41:04 +0200

    trzeci commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Pierwszy commit");
         System.out.println("Drugi commit");
+        System.out.println("Drugi commit");
     }
 }
